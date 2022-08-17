aa8f219f36194edc3271af1d4a6c13179a03511a
Thammathada10 <106522326+Thammathada10@users.noreply.github.com>
2022-08-17 09:12:43 +0700

    Add files via upload

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -25,5 +25,19 @@ window.addEventListener('DOMContentLoaded', () => {
             }
         }
         scrollPos = currentTop;
+        exports.Util = Util;
+        exports.Alert = Alert;
+        exports.Button = Button;
+        exports.Carousel = Carousel;
+        exports.Collapse = Collapse;
+        exports.Dropdown = Dropdown;
+        exports.Modal = Modal;
+        exports.Popover = Popover;
+        exports.Scrollspy = ScrollSpy;
+        exports.Tab = Tab;
+        exports.Toast = Toast;
+        exports.Tooltip = Tooltip;
+      
+        Object.defineProperty(exports, '__esModule', { value: true });
     });
 })
